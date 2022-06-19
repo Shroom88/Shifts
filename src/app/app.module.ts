fb35dc79e69afe09162ad672aa145748e0d2d46d
Gabriel Stanev <47984623+Shroom88@users.noreply.github.com>
2022-06-19 16:01:26 +0300

    header design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
